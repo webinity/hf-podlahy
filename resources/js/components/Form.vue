035865fc85386bd2502bb2e6816f67e4b0b3fd02
Vojtech Rerabek <vrerabek@gmail.com>
2024-04-19 00:45:53 +0200

    working form

--- resources/js/components/Form.vue
+++ resources/js/components/Form.vue
@@ -1,45 +1,93 @@
 <template>
-    <form action="POST">
+    <form>
         <div class="name col">
             <label for="name">Jméno a příjmení:</label>
-            <input type="text" name="name" id="" required>
+            <input v-model="name" type="text" name="name" id="" required>
         </div>
         <div class="tel-email">
             <div class="col">
                 <label for="email">E-mail:</label>
-                <input type="email" name="email" id="">
+                <input v-model="email" type="email" name="email" id="">
             </div>
 
             <div class="col">
                 <label for="tel">Telefonní číslo:</label>
-                <input type="text" name="tel" id="" required>
+                <input v-model="phone" type="text" name="tel" id="" required>
             </div>
         </div>
 
         <div class="msg col">
             <label for="textarea">Dotaz / poptávka:</label>
-            <textarea name="textarea" id="" cols="30" rows="10" required></textarea>
+            <textarea v-model="message" name="textarea" id="" cols="30" rows="10" required></textarea>
         </div>
 
-        <button type="submit">
+        <button @click="submit" type="button" id="submit">
             <span>Odeslat</span>
         </button>
 
-        <div class="success"><p>Vaše zpráva byla úspěšně doručena! Brzy Vám ozveme.</p></div>
-        <div class="error"><p>Omlouváme se, ale něco se pokazilo. Zkuste nám napsat přímo na email <a href="mailto:info@hfpodlahy.cz">info@hfpodlahy.cz</a>.</p></div>
-        <div class="error required">
-            <p>Tato pole jsou povinná a Vámi nevyplněná:</p>
+        <div class="success" id="success-alert"><p>Vaše zpráva byla úspěšně doručena! Brzy Vám ozveme.</p></div>
+        <!-- <div class="error"><p>Omlouváme se, ale něco se pokazilo. Zkuste nám napsat přímo na email <a href="mailto:info@hfpodlahy.cz">info@hfpodlahy.cz</a>.</p></div> -->
+        <div class="error required" id="error-alert">
+            <!-- <p>Tato pole jsou povinná a Vámi nevyplněná:</p>
             <ul>
                 <li>neco</li>
-            </ul>
+            </ul> -->
         </div>
     </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default ({
     data() {
-        return {}
+        return {
+            name: null,
+            message: null,
+            phone: null,
+            email: null
+        }
+    },
+
+    methods: {
+        submit() { 
+            let submitBtn = document.getElementById('submit')
+            let successAlert = document.getElementById('success-alert')
+            let errorAlert = document.getElementById('error-alert')
+
+            submitBtn.innerHTML = 'ODESÍLÁME...';
+            successAlert.style.display = 'none';
+            errorAlert.style.display = 'none';
+
+            axios
+            .post('/form', {
+              name: this.name,
+              message: this.message,
+              email: this.email,
+              phone: this.phone
+            })
+            .then(response => {
+              submitBtn.innerHTML = 'Odeslat zprávu';
+              successAlert.style.display = 'block';
+              this.message = null;
+              this.name = null;
+              this.phone = null;
+              this.email = null;
+            })
+            .catch(function (error) {
+              // handle error
+              submitBtn.innerHTML = 'ODESLAT';
+              var arr = error.response.data;
+              var err = '';
+              for (let el in arr) {
+                if (arr.hasOwnProperty(el)) {
+                  err = err + '<li>' + arr[el] + '</li>';
+                }
+              }
+              errorAlert.innerHTML = err;
+              errorAlert.style.display = 'block';
+            })
+        }
     }
 })
 </script>
